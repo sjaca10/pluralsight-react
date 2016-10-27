b72553fffd37db2e13794428f5828a779576ec8b
Javier Aguila <sjaca10@gmail.com>
2016-10-27 17:52:42 -0500

    Adding toastr package to show a fancy message to the user.

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -4,6 +4,7 @@ var React = require('react');
 var Router = require('react-router');
 var AuthorForm = require('./authorForm');
 var AuthorApi = require('../../api/AuthorApi');
+var toastr = require('toastr');
 
 var ManageAuthorPage = React.createClass({
     mixins: [
@@ -30,6 +31,7 @@ var ManageAuthorPage = React.createClass({
     saveAuthor: function(event) {
         event.preventDefault();
         AuthorApi.saveAuthor(this.state.author);
+        toastr.success('Author saved.');
         this.transitionTo('authors');
     },
 
