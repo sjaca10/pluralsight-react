2565227469752bde2769730f059058d7174c5391
Javier Aguila <sjaca10@gmail.com>
2016-10-27 16:45:28 -0500

    Link up form to author and author page manager to share the state of the form

--- src/components/authors/authorForm.js
+++ src/components/authors/authorForm.js
@@ -9,11 +9,23 @@ var AuthorForm = React.createClass({
             <form>
                 <h1>Manage Author</h1>
                 <label htmlFor="firstName">First Name</label>
-                <input type="text" name="firstName" className="form-control" placeholder="First Name" ref="firstName" value="" />
+                <input type="text"
+                    name="firstName"
+                    className="form-control"
+                    placeholder="First Name"
+                    ref="firstName"
+                    onChange={this.props.onChange}
+                    value={this.props.author.firstName} />
                 <br />
 
                 <label htmlFor="lastName">Last Name</label>
-                <input type="text" name="lastName" className="form-control" placeholder="Last Name" ref="lastName" value="" />
+                <input type="text"
+                    name="lastName"
+                    className="form-control"
+                    placeholder="Last Name"
+                    ref="lastName"
+                    onChange={this.props.onChange}
+                    value={this.props.author.lastName} />
                 <br />
 
                 <input type="submit" value="Save" className="btn btn-default" />
