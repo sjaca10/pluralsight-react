bb2deedb7433bb3fc24fb39c9dcd61b24dcbda2c
Javier Aguila <sjaca10@gmail.com>
2016-10-27 12:39:22 -0500

    Adding NotFoundPage with React Router URL.

--- src/components/homePage.js
+++ src/components/homePage.js
@@ -2,7 +2,7 @@
 
 var React = require('react');
 var Router = require('react-router');
-var Link = Router.Link;
+var Link = require('react-router').Link;
 
 var Home = React.createClass({
     render: function() {
