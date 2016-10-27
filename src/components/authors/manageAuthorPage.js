b7961a4b6b5ec6ff134b010371cd87b84b550cea
Javier Aguila <sjaca10@gmail.com>
2016-10-27 15:50:28 -0500

    Starting add a basic form to add an author.

---
+++ src/components/authors/manageAuthorPage.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var React = require('react');
+var AuthorForm = require('./authorForm');
+
+var ManageAuthorPage = React.createClass({
+    render: function() {
+        return (
+            <AuthorForm />
+        );
+    }
+});
+
+module.exports = ManageAuthorPage;
